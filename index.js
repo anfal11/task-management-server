820f52fd828b8e2da051a8f0b4af315e55a2cb0a
anfal11 <anfal15-14303@diu.edu.bd>
2023-12-22 01:46:19 +0600

    put method added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -58,7 +58,20 @@ async function run() {
         const result = await taskCollection.insertOne(newTask);
         res.send(result);
             })
-
+            
+    app.delete("/tasks/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: new ObjectId(id) };
+        const result = await taskCollection.deleteOne(query);
+        res.send(result);
+      });
+      app.put("/tasks/:id", async (req, res) => {
+        const id = req.params.id;
+        const filter = { _id: new ObjectId(id) };
+        const updatedDoc = { $set: req.body };
+        const result = await taskCollection.updateOne(filter, updatedDoc);
+        res.send(result);
+      });
 
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
